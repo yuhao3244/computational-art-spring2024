1b72eb1639904083554eef21bc11a01727c55f19
yuhao3244 <145723746+yuhao3244@users.noreply.github.com>
2024-02-01 03:21:13 -0500

    Assignment 1: Github Configuration, and a work of abstract art using p5
    The final version of the assignment

--- assignments/01_abstract/sketch.js
+++ assignments/01_abstract/sketch.js
@@ -1,36 +1,26 @@
 let dots = [];
-let numDots = 1000;
-let gravity = 0.098;
+let numDots = 600;
+let gravity = 3;
 
-// p5 calls this function right away when the webpage is loaded
 function setup() {
   createCanvas(800, 600);
   colorMode(HSB);
   noStroke();
   
-  // Create an array of Dot objects (see the class below)
   for (let i = 0; i < numDots; i++) {
-    // Make use of the iterator variable (i) to compute the x position of the dot
     let x = width / numDots * i;
-    dots[i] = new Dot(x, 0, 10, i);
+    dots[i] = new Dot(3*x, x, 10, i);
   }
 }
 
-// The draw function is called over and over again really fast by p5
 function draw() {
   background(0, 0, 100, 0.08);
 
-  // Loop through the array of dots that we created on inside the start function, and 
-  // call their update function.
   for (let i = 0; i < dots.length; i++) {
     dots[i].update();
   }
 }
 
-
-
-// Below, we define the Dot class, which defined the objects we instantiate inside the
-// setup function above.
 class Dot {
   constructor(x, y, diameter, id) {
 
@@ -45,7 +35,6 @@ class Dot {
     this.hue = this.id / numDots * 360;
   }
 
-  // This is the function that is called in the draw function above.
   update() {
     this.position.x += random(-5, 5);
 
