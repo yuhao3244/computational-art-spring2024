02b369bedcf53d04c36ea8dfe9c21cde41419fe2
yuhao3244 <145723746+yuhao3244@users.noreply.github.com>
2024-02-01 04:01:27 -0500

    01_abstract
    The real final version!

--- assignments/01_abstract/sketch.js
+++ assignments/01_abstract/sketch.js
@@ -1,5 +1,5 @@
 let dots = [];
-let numDots = 600;
+let numDots = 400;
 let gravity = 3;
 
 function setup() {
@@ -9,7 +9,7 @@ function setup() {
   
   for (let i = 0; i < numDots; i++) {
     let x = width / numDots * i;
-    dots[i] = new Dot(3*x, x, 10, i);
+    dots[i] = new Dot(3 * x, x, 10, i);
   }
 }
 
@@ -22,32 +22,32 @@ function draw() {
 }
 
 class Dot {
-  constructor(x, y, diameter, id) {
-
+  constructor(x, y, size, id) {
     this.position = createVector(x, y);
-
     this.velocity = createVector(0, 0);
-
     this.id = id;
-
-    this.diameter = diameter + random(-15, 15);
-
+    this.size = size + random(-15, 15);
     this.hue = this.id / numDots * 360;
   }
 
   update() {
     this.position.x += random(-5, 5);
-
     this.velocity.y += gravity;
-
     this.position.add(this.velocity);
 
-    if (this.position.y + this.diameter/2 > height) {
+    if (this.position.y + this.size / 2 > height) {
       this.velocity.y *= -1;
       this.position.add(this.velocity);
     }
 
     fill(this.hue, 50, 100);
-    ellipse(this.position.x, this.position.y, this.diameter, this.diameter);
+    let x1 = this.position.x;
+    let x2 = this.position.x - this.size / 2;
+    let x3 = this.position.x + this.size / 2;
+
+    let y1 = this.position.y + this.size / 2;
+    let y2 = this.position.y - this.size / 2;
+    let y3 = this.position.y - this.size / 2;
+    triangle(x1, y1, x2, y2, x3, y3);
   }
 }
