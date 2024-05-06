04d211ceaad184d4ab0cf4582abe9baaf8809e48
yuhao3244 <145723746+yuhao3244@users.noreply.github.com>
2024-05-06 05:51:50 -0400

    7

--- assignments/07_fractals/sketch.js
+++ assignments/07_fractals/sketch.js
@@ -2,19 +2,30 @@ function setup() {
   createCanvas(600, 600);
   colorMode(HSB);
   background(0, 0, 0, 1);
+  noStroke();
 }
 
 function draw() {
-  fill(frameCount % 360, 80, 100, 0.1); 
-  translate(width / 2, height / 2);
-  rotate(radians(frameCount));
-  drawCircle(0, 0, 300);
+  drawFractal(width / 2, height / 2, 300);
 }
 
-function drawCircle(x, y, d) {
+function drawFractal(x, y, d) {
   ellipse(x, y, d);
-  if (d > 5) {
-    drawCircle(x + d * 0.5, y, d * 0.5);
-    drawCircle(x - d * 0.5, y, d * 0.5);
+  rect(x, y, d * 0.8, d * 0.8);
+  triangle(x, y, x + d / 2, y + d, x - d / 2, y + d);
+
+  if (mouseIsPressed) {
+    fill(random(360), 80, 100, 0.1);
+  } else {
+    fill(frameCount % 360, 80, 100, 0.1);
   }
+
+  if (d > 5) {
+    drawFractal(x + d * 0.5, y, d * 0.5);
+    drawFractal(x - d * 0.5, y, d * 0.5);
+  }
+}
+
+function mouseClicked() {
+  background(0);
 }
