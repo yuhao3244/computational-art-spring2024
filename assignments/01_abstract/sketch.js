3efbee0f5bb122bccfeb2e7bc53c31a5d07cc169
yuhao3244 <145723746+yuhao3244@users.noreply.github.com>
2024-05-06 04:49:27 -0400

    1

--- assignments/01_abstract/sketch.js
+++ assignments/01_abstract/sketch.js
@@ -1,53 +1,72 @@
-let dots = [];
-let numDots = 400;
-let gravity = 3;
+let shapes = []; 
 
 function setup() {
   createCanvas(800, 600);
-  colorMode(HSB);
-  noStroke();
   
-  for (let i = 0; i < numDots; i++) {
-    let x = width / numDots * i;
-    dots[i] = new Dot(3 * x, x, 10, i);
+  // 创建一些初始形状
+  for (let i = 0; i < 100; i++) {
+    let x = random(width);
+    let y = random(height);
+    let size = random(20, 80);
+    let shapeType = int(random(3)); // 随机选择形状
+    shapes.push(new Shape(x, y, size, shapeType));
   }
 }
 
 function draw() {
-  background(0, 0, 100, 0.08);
-
-  for (let i = 0; i < dots.length; i++) {
-    dots[i].update();
+  background(240);
+  
+  // 绘制和更新形状
+  for (let i = 0; i < shapes.length; i++) {
+    shapes[i].display();
+    shapes[i].move();
   }
 }
 
-class Dot {
-  constructor(x, y, size, id) {
-    this.position = createVector(x, y);
-    this.velocity = createVector(0, 0);
-    this.id = id;
-    this.size = size + random(-15, 15);
-    this.hue = this.id / numDots * 360;
+function mousePressed() {
+  // 鼠标点击时改变所有形状的颜色
+  for (let i = 0; i < shapes.length; i++) {
+    shapes[i].changeColor();
   }
+}
 
-  update() {
-    this.position.x += random(-5, 5);
-    this.velocity.y += gravity;
-    this.position.add(this.velocity);
-
-    if (this.position.y + this.size / 2 > height) {
-      this.velocity.y *= -1;
-      this.position.add(this.velocity);
+class Shape {
+  constructor(x, y, size, type) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.type = type; // 形状类型：0=圆形，1=矩形，2=三角形
+    this.color = color(random(255), random(255), random(255)); 
+    this.speedX = random(-2, 2);
+    this.speedY = random(-2, 2);
+  }
+  
+  display() {
+    fill(this.color);
+    noStroke();
+    if (this.type === 0) {
+      ellipse(this.x, this.y, this.size);
+    } else if (this.type === 1) {
+      rect(this.x, this.y, this.size, this.size);
+    } else if (this.type === 2) {
+      triangle(this.x, this.y - this.size / 2, this.x - this.size / 2, this.y + this.size / 2, this.x + this.size / 2, this.y + this.size / 2);
     }
-
-    fill(this.hue, 50, 100);
-    let x1 = this.position.x;
-    let x2 = this.position.x - this.size / 2;
-    let x3 = this.position.x + this.size / 2;
-
-    let y1 = this.position.y + this.size / 2;
-    let y2 = this.position.y - this.size / 2;
-    let y3 = this.position.y - this.size / 2;
-    triangle(x1, y1, x2, y2, x3, y3);
+  }
+  
+  move() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    
+    // 边界检测
+    if (this.x < 0 || this.x > width) {
+      this.speedX *= -1;
+    }
+    if (this.y < 0 || this.y > height) {
+      this.speedY *= -1;
+    }
+  }
+  
+  changeColor() {
+    this.color = color(random(255), random(255), random(255));
   }
 }
